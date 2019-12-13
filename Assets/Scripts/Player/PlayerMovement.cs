de68deb96a10fc5753893270b16aebecd35e1c55
Sebastien Gaumin <sgaumin@gmail.com>
2019-12-13 23:50:39 +0100

    [Feature] Player movement.

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+	[SerializeField] private float movementSpeed = 100f;
+
+	private Vector3 movement;
+	private Rigidbody body;
+
+	protected void Start()
+	{
+		body = GetComponent<Rigidbody>();
+	}
+
+	private void Update()
+	{
+		movement.x = Input.GetAxis("Horizontal");
+		movement.z = Input.GetAxis("Vertical");
+
+		body.AddForce(movement * movementSpeed * Time.deltaTime);
+	}
+}
