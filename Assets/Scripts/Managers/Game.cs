8228caf2c386ebd392e5648ac0a450b2cd0c2257
Sébastien Gaumin <sgaumin@gmail.com>
2019-12-13 17:57:44 +0100

    Initial commit

---
+++ Assets/Scripts/Managers/Game.cs
@@ -0,0 +1,29 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Game : GameSystem
+{
+	public static Game Instance { get; private set; }
+
+	public GameStates GameState { get; private set; } = GameStates.Play;
+
+	protected override void Awake()
+	{
+		base.Awake();
+		Instance = this;
+	}
+
+	protected override void Update()
+	{
+		base.Update();
+	}
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# TemplateGJ_2D
+Project template for 2D prototype or game jam.
+
+For Unity 2019.1.9f1.
