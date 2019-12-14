41ad0047045770ba87e17434aea84d78d58a9b5b
Sebastien Gaumin <sgaumin@gmail.com>
2019-12-14 15:50:58 +0100

    [Build] Quit Game in Menu scene implemented.

--- Assets/Scripts/Managers/StartMenuManager.cs
+++ Assets/Scripts/Managers/StartMenuManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StartMenuManager : MonoBehaviour
+public class StartMenuManager : GameSystem
 {
 	private void Start() => FadScreen.Instance.FadIn(Color.black);
 
@@ -12,5 +12,10 @@ public class StartMenuManager : MonoBehaviour
 	{
 		yield return FadScreen.Instance.FadOutCore(Color.black);
 		LevelLoader.LoadNextLevel();
+	}
+
+	protected override void Update()
+	{
+		base.Update();
 	}
 }
