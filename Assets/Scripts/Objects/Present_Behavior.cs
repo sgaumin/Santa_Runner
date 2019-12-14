12b94c9c33219d11194421dd97d95dd8e231b429
Bo <zboyang@umich.edu>
2019-12-13 20:54:49 -0500

    Destroy presents when they're offscreen

---
+++ Assets/Scripts/Objects/Present_Behavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Present_Behavior : MonoBehaviour
+{
+    // Delete self when invisible to clear up memory resources
+    private void OnBecameInvisible()
+    {
+        Debug.Log("Destroying present");
+        Destroy(this.gameObject);
+    }
+}
