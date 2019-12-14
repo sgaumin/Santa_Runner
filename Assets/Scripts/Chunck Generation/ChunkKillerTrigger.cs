cb28c2b721207958b9aad6f7a8f8d83346c2a5d7
Sebastien Gaumin <sgaumin@gmail.com>
2019-12-14 02:08:22 +0100

    [Fix] Destroying chunks after spawning.

---
+++ Assets/Scripts/Chunck Generation/ChunkKillerTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkKillerTrigger : MonoBehaviour
+{
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("EndChunk"))
+		{
+			Destroy(other.transform.parent.gameObject);
+		}
+	}
+}
