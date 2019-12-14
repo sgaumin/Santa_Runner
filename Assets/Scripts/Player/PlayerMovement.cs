dbac5a91ccbc78351111ea4f023238cf7d2ceb8e
Bo <zboyang@umich.edu>
2019-12-13 21:38:53 -0500

    [Feature] Player does not move off screen

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 	[SerializeField] private float movementSpeed = 100f;
+    [SerializeField] private float screenBoundaryBuffer = 20f; // Adjust how far off screen player can go
     [SerializeField] private string collisionTag = "Building";
 
     private Vector3 movement;
@@ -22,7 +23,35 @@ public class PlayerMovement : MonoBehaviour
 		movement.z = Input.GetAxis("Vertical");
 
 		body.AddForce(movement * movementSpeed * Time.deltaTime);
-	}
+
+        // Only move player if on screen, else stop the play on screen boundaries IF the player has enough velocity to push past boundary
+        // Also reset the moving velocity in the direction of collision to allow faster recovery in opposite direction
+        Vector3 player_screen = Camera.main.WorldToScreenPoint(transform.position);
+        float original_y = transform.position.y; 
+        Debug.Log(player_screen);
+        if (player_screen.x < screenBoundaryBuffer && body.velocity.x < 0)
+        {
+            player_screen.x = screenBoundaryBuffer;
+            body.velocity = new Vector3(0, body.velocity.y, body.velocity.z);
+        }
+        else if (player_screen.x > (Screen.width - screenBoundaryBuffer) && body.velocity.x > 0)
+        {
+            player_screen.x = Screen.width - screenBoundaryBuffer;
+            body.velocity = new Vector3(0, body.velocity.y, body.velocity.z);
+        }
+        if (player_screen.y < screenBoundaryBuffer && body.velocity.z < 0)
+        {
+            player_screen.y = screenBoundaryBuffer;
+            body.velocity = new Vector3(body.velocity.x, body.velocity.y, 0);
+        }
+        else if (player_screen.y > (Screen.height - screenBoundaryBuffer) && body.velocity.z > 0)
+        {
+            player_screen.y = Screen.height - screenBoundaryBuffer;
+            body.velocity = new Vector3(body.velocity.x, body.velocity.y, 0);
+        }
+        transform.position = Camera.main.ScreenToWorldPoint(player_screen);
+        transform.position = new Vector3(transform.position.x, original_y, transform.position.z); // Make sure player doesn't move up or down
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
