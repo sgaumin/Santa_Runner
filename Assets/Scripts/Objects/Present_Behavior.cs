ff83614dffe4c80543eb3eaa9e984ff89ee794cd
Bo <zboyang@umich.edu>
2019-12-13 20:58:09 -0500

    [Cleanup] Raycast present throw code changed from loop to if, shouldn't need multiple throws of a present

--- Assets/Scripts/Objects/Present_Behavior.cs
+++ Assets/Scripts/Objects/Present_Behavior.cs
@@ -7,7 +7,7 @@ public class Present_Behavior : MonoBehaviour
     // Delete self when invisible to clear up memory resources
     private void OnBecameInvisible()
     {
-        Debug.Log("Destroying present");
+        //Debug.Log("Destroying present");
         Destroy(this.gameObject);
     }
 }
