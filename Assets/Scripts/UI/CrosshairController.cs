f007a1dda7af0167251a2053f3b2b2b846cc75c2
evanmalmud <evanmalmud@gmail.com>
2019-12-14 15:47:25 -0500

    Adding in Crosshair

---
+++ Assets/Scripts/UI/CrosshairController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrosshairController : MonoBehaviour
+{
+    [SerializeField] private GameObject crosshair;
+    [SerializeField] private Canvas canvas;
+    private Vector3 MouseCoords;
+
+    public float MouseSensitivity = 0.1f;
+
+    private void Start()
+    {
+            Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector2 pos;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, Input.mousePosition, canvas.worldCamera, out pos);
+        crosshair.transform.position = canvas.transform.TransformPoint(pos);
+    }
+}
