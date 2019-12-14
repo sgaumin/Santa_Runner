b371f1f35d69bc6710cc1469396442873e807f70
Sebastien Gaumin <sgaumin@gmail.com>
2019-12-14 12:02:43 +0100

    [Build] Creation of Menu scene.

---
+++ Assets/Scripts/Managers/StartMenuManager.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenuManager : MonoBehaviour
+{
+	public void LoadGame() => LevelLoader.LoadNextLevel();
+}
