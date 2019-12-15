6ce9bb7343376959e5a30599c161c17a59968f52
Sebastien Gaumin <sgaumin@gmail.com>
2019-12-15 18:53:45 +0100

    [Polish] Destroy animation on Present object.

--- Assets/Scripts/Objects/PresentBehavior.cs
+++ Assets/Scripts/Objects/PresentBehavior.cs
@@ -1,14 +1,16 @@
-﻿using System.Collections;
+﻿using DG.Tweening;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PresentBehavior : MonoBehaviour
 {
-	[SerializeField] private bool isUsedAsDecoration;
-
 	[SerializeField] private int points_increment = 1;
 	[SerializeField] private int wind_scale = 50;
 	[SerializeField] private string scoreColliderTag = "Chimney";
+
+	[Space]
+	[SerializeField] private float timeBeforeDestruction = 5f;
 
 	[Header("References")]
 	[SerializeField] private ParticleSystem hitEffect;
@@ -18,16 +20,17 @@ public class PresentBehavior : MonoBehaviour
 
 	private void Awake() => body = GetComponent<Rigidbody>();
 
-	private void Start() => models.Random().gameObject.SetActive(true);
+	private void Start()
+	{
+		models.Random().gameObject.SetActive(true);
+		StartCoroutine(LaunchDestroyAnimation());
+	}
 
 	private void Update()
 	{
-		if (!isUsedAsDecoration)
-		{
-			// Apply wind, can disable on Wind object inspector
-			Vector3 wind_vel = WindBehavior.Instance.GetWindVel();
-			body.AddForce(wind_vel * Time.deltaTime * wind_scale);
-		}
+		// Apply wind, can disable on Wind object inspector
+		Vector3 wind_vel = WindBehavior.Instance.GetWindVel();
+		body.AddForce(wind_vel * Time.deltaTime * wind_scale);
 	}
 
 	private void OnCollisionEnter(Collision collision)
@@ -42,5 +45,13 @@ public class PresentBehavior : MonoBehaviour
 				Destroy(this.gameObject);
 			}
 		}
+	}
+
+	private IEnumerator LaunchDestroyAnimation()
+	{
+		yield return new WaitForSeconds(timeBeforeDestruction);
+		transform.DOScale(0f, 1f).SetEase(Ease.InBounce).Play();
+		yield return new WaitForSeconds(1f);
+		Destroy(gameObject);
 	}
 }
