ae9d6834f87dc46fdbc568a6728770c8326295df
Bo <zboyang@umich.edu>
2019-12-15 00:23:30 -0500

    [Integration, Level] Added in child model (no animation). Added more children to Chunk1

---
+++ Assets/Scripts/Objects/ChildTransform.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildTransform : MonoBehaviour
+{
+    private void Update()
+    {
+        Quaternion currentAngles = new Quaternion();
+        currentAngles.eulerAngles = new Vector3(-90, transform.rotation.eulerAngles.y, 0);
+        transform.rotation = currentAngles;
+    }
+}
