6622cdc3dfd6f904e0d6ce57c2f973e87c294b18
Sebastien Gaumin <sgaumin@gmail.com>
2019-12-15 11:16:18 +0100

    [Audio] Santa Claus voice added.

---
+++ Assets/Scripts/Player/PlayerVoice.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PlayerVoice : MonoBehaviour
+{
+	[SerializeField] private AudioExpress voiceSound;
+	[MinMaxSlider(1f, 20f), SerializeField] private MinMax breakDuration = new MinMax(6f, 12f);
+
+	private Coroutine voiceSoundPlaying;
+
+	private void Awake()
+	{
+		Game.Instance.OnStartGame += PlaySound;
+		Game.Instance.OnGameOver += StopSound;
+	}
+
+	public void PlaySound() => voiceSoundPlaying = StartCoroutine(PlaySoundCore());
+
+	public void StopSound() => StopCoroutine(voiceSoundPlaying);
+
+	private IEnumerator PlaySoundCore()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(breakDuration.RandomValue);
+			voiceSound.Play(gameObject);
+		}
+	}
+
+	private void OnDestroy()
+	{
+		Game.Instance.OnStartGame -= PlaySound;
+		Game.Instance.OnGameOver -= StopSound;
+	}
+}
