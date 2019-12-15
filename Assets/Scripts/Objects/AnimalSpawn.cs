1ee3d4eea590950dd066d6e47725fffe5b9d3204
Bo <zboyang@umich.edu>
2019-12-14 19:33:44 -0500

    [Fix] Moved world down. Changed animal colliders to trigger.

--- Assets/Scripts/Objects/AnimalSpawn.cs
+++ Assets/Scripts/Objects/AnimalSpawn.cs
@@ -6,13 +6,20 @@ public class AnimalSpawn : MonoBehaviour
 {
     [SerializeField] private GameObject to_instantiate;
 
+    private GameObject inst;
+
     private void Start()
     {
         // Needs to be in start, since NavMesh needs to be set
-        GameObject inst = Instantiate(to_instantiate, this.transform.parent);
+        inst = Instantiate(to_instantiate, this.transform.parent);
         //Debug.Log(inst.transform.position);
 
         // Deactivate this spawner after usage
         this.gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        Destroy(inst);
     }
 }
